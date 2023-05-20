42b0ad1164ab733d0bea449d2aa10fc140e3abb7
Milena.Lopes.Mesquita.Alcântara <milena.alcantara@zup.com.br>
2023-05-20 15:12:38 -0300

    Alterações conforme o feedback

--- JogoDeDados/Melhorias/Main.java
+++ JogoDeDados/Melhorias/Main.java
@@ -1,80 +1,89 @@
+package Melhorias;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+
 public class Main {
 
     public static Scanner entdaddos = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Bem vindo(a)!");
-        System.out.println(" O Jogo começou :D \n Você tem 3 chances!");
+        System.out.println(" O Jogo começou :D");
         jogo();
-
+        mostrarPontucao();
+        finalizarJogo();
     }
-
-    public static int numeroDoUser,pontuacao1 = 0, pontode10 = 0, pontuacao2 = 0, pontode5 = 0, rounds = 3;
+    public static int numeroDoUser,pontuacao1 = 0, pontode10 = 0, pontuacao2 = 0, pontode5 = 0, rounds, qtAcertos, zerado, pontuacao3;
+    public static  int totalPontos;
     public static List<Integer> pontos = new ArrayList<>();
     public static void jogo(){
-        for (int i = 0; i <3 ; i++) {
+        System.out.println("Quantas rodadas deseja jogar?");
+        rounds = entdaddos.nextInt();
+        for (int i = 0; i <rounds ; i++) {
             System.out.println("------------------------------------");
-            System.out.println(" Informe um número: ");
+            System.out.println(" Informe um número de 1 a 6: ");
             numeroDoUser = entdaddos.nextInt();
 
             if (numeroDoUser >= 1 && numeroDoUser <= 6) {
                 Random random = new Random();
-                int numeroAleatorio = random.nextInt(6) + 1;
+                int numeroAleatorio = random.nextInt(6)+1 ;
                 System.out.println(" O número sorteado foi: " + numeroAleatorio);
                 if (numeroDoUser == numeroAleatorio) {
                     System.out.println(" Uau que sorte!\n Seu número é igual ao sorteado, você ganhou 10 pontos");
                     pontuacao1 = 10;
+                    pontos.add(pontuacao1);
                     System.out.println("Pontuação atual: " + pontuacao1);
                     pontode10++;
-                    pontos.add(pontuacao1);
                 } else if (numeroDoUser + 1 == numeroAleatorio || numeroDoUser - 1 == numeroAleatorio) {
                     System.out.println("Seu número é diferente em 1 unidade do sorteado, Parabéns! Você ganhou 5 pontos");
                     pontuacao2 = 5;
+                    pontos.add(pontuacao2);
                     System.out.println("Pontuação atual: " + pontuacao2);
                     pontode5++;
-                    pontos.add(pontuacao2);
                 } else {
                     System.out.println("Ops :/  Não foi dessa vez");
-                    int pontuacao3 = 0;
+                    pontuacao3 = 0;
+                    zerado++;
                     System.out.println("Pontuação atual: " + pontuacao3);
                 }
             } else {
                 System.out.println("Numero Inválido");
-                System.exit(0);
             }
-}
-        mostrarPontucao();
+        }
     }
     public static void mostrarPontucao(){
-        int totaldepontos= 0;
         System.out.println("-------------------------------------");
         System.out.println("Suas chances acabaram! Visualize seu placar: ");
         for (int i = 0; i <pontos.size() ; i++) {
-           totaldepontos += pontos.get(i);
+            totalPontos += pontos.get(i);
         }
-        System.out.println("Sua pontuação total é: "+totaldepontos);
-
+        System.out.println("Sua pontuação total é: "+totalPontos);
             if(pontode10+pontode5 == rounds){
                 System.out.println("Voce acertou em todas as rodadas");
-            } else if (pontode10 +pontode5 == 0 ) {
+            } else if (zerado == rounds ) {
                 System.out.println("Que pena, você não acertou o número do dado em nenhuma rodada :(");
             } else{
-              System.out.println("Voce acertou em "+(pontode10+pontode5)+ " rodadas e errou em  "+ (rounds - (pontode10+pontode5)));
+              System.out.println("Voce acertou "+( qtAcertos= pontode10+pontode5)+ " vezes e errou "+ (zerado)+" vez(es)");
             }
-        finalizarJogo();
+
     }
     public static void finalizarJogo(){
         System.out.println("--------------------------");
-        System.out.println("Deseja iniciar outra rodada? ");
+        System.out.println("Deseja reiniciar o jogo? ");
         System.out.println("Informe 1 - SIM \t 2 - NÃO");
         int decisao = entdaddos.nextInt();
         switch (decisao){
             case 1:
+                totalPontos=0;
+                pontode5 =0;
+                pontode10 = 0;
+                rounds = 0;
                 jogo();
+                mostrarPontucao();
+                finalizarJogo();
             break;
 
             case 2:
